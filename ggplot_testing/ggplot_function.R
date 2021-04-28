fece7dec3463ab51795968a40f58e7a16604f177
Morrissey <David.Morrissey@ons.gov.uk>
2021-04-28 12:27:42 +0100

    Cleaned up function
    Created R project for Explorer app project
    
    David
    
    Signed-off-by: Morrissey <David.Morrissey@ons.gov.uk>

--- ggplot_testing/ggplot_function.R
+++ ggplot_testing/ggplot_function.R
@@ -2,57 +2,33 @@ library(tidyverse)
 library(ggplot2)
 library(dplyr)
 
+#change name later
+
 threeaone_function_test <- read.csv("H:\\3-a-1 (1).csv", na.strings=c("","NA")) %>% 
   mutate_if(is.factor, as.character)
 
 
-color_select <- "Sex" # Color
-facet1 <- "Age" # Facet wrap
-facet2 <- "Country" # Facet wrap
-
-
-simple_plot <- function(df, color_select, facet1, facet2) {
-  
-  # test <- ggplot(data = df, 
-  #                aes(Year, 
-  #                    Value, 
-  #                    color = color_select))+
-  #   geom_line()+
-  #   geom_point(color = "steelblue") +
-  #   labs(title = "Percentage of Men in all regions of England of all age ranges who are current cigarette smokers aged 18 years and older",
-  #        y = "Percentage(%)", x = "") +
-  #   facet_grid(facet1 ~ facet2)
-  # return(test)
-}
-
-test_facet_args <- function(df, color_select, facet1, facet2) {
+test_facet_args <- function(df, facet1, facet2, linecolour) {
   
 
   df <- df %>% 
     mutate(facet1 = !!(enquo(facet1)),
-           facet2 = !!(enquo(facet2)))
+           facet2 = !!(enquo(facet2)),
+           linecolour = !!(enquo(linecolour)))
   
   ggplot(data = df, 
-         aes(Year, Value, color = color_select))+
-    geom_line() +
-    geom_point(color = "steelblue")+
+         aes(Year, Value))+
+    geom_line(aes(colour = linecolour)) +
+    geom_point()+
     labs(title = "Percentage of Men in all regions of England of all age ranges who are current cigarette smokers aged 18 years and older",
          y = "Percentage(%)", x = "") +
     facet_grid(facet1 ~ facet2)
   
 }
 
-plot <- test_facet_args(threeaone_function_test, Sex, Age, Country)
+ test_facet_args(threeaone_function_test,
+                 linecolour = Sex, 
+                 facet1 = Age, 
+                 facet2 = Country)
 
-test_plot <- simple_plot(threeaone_function_test, color_select = Sex, facet1 = Age, facet2 = Country)
-
-
-
-
-ggplot(data = threeaone_function_test, aes(Year, Value, color = Sex))+
-  geom_line()+
-  geom_point(color = "steelblue")+
-  labs(title = "Percentage of Men in all regions of England of all age ranges who are current cigarette smokers aged 18 years and older",
-       y = "Percentage(%)", x = "")+
-  facet_grid(Age ~ Country)
 
