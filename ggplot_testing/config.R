7b92fafb2eb10be537ae38120854cbb285d11998
Morrissey <David.Morrissey@ons.gov.uk>
2021-04-28 16:45:30 +0100

    Added config and filtering script
    Signed-off-by: Morrissey <David.Morrissey@ons.gov.uk>

---
+++ ggplot_testing/config.R
@@ -0,0 +1,14 @@
+
+csv_filepath <- "Y:\\Data Collection and Reporting\\Jemalex\\CSV\\"
+indicator <- "indicator_3-a-1.csv"
+char1 <- "Sex"
+char2 <- "Country"
+char3 <- NA
+char4 <- NA
+disagg1 <- c(NA, "Male")
+disagg2 <- c("England")
+disagg3 <- NA
+disagg4 <- NA
+line_colour <- "Sex"
+facet_row <- "Country"
+facet_column <- NA
