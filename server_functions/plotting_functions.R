b77f8e181cdd5d59c4833b02298559df3747c378
Jakki <Varun.Jakki@ons.gov.uk>
2021-05-26 17:12:39 +0100

    Changes to plotting_functions
    - attempting two variations of functioning out base plot
    - 1) create base plot and then add to this within the function as seen in test_facet_args_grid
    - 2) adding function to the ggplot - lines 31 to 34
    
    Signed-off-by: Jakki <Varun.Jakki@ons.gov.uk>

--- server_functions/plotting_functions.R
+++ server_functions/plotting_functions.R
@@ -1,22 +1,39 @@
+# This function creates a basic plot plotting X against y as a base
+basic_plot <- function(df){
+  ggplot(data = filtered_data,
+                     aes(Year, Value)) +
+  geom_point()
+}
+
+
+basic_plot_start <- basic_plot(filtered_data)
+basic_plot_start
+
 #This function is for facet wrapping. Will only facet by row
 test_facet_args <- function(df, facet2, linecolour) {
   
   facet2 <- sym(facet2)
   linecolour <- sym(linecolour)
   
-  df <- df %>% 
+  df <- df %>%
     mutate(facet2 = !!(enquo(facet2)),
            linecolour = !!(enquo(linecolour)))
   
-  ggplot(data = df, 
-         aes(Year, Value))+
-    geom_line(aes(colour = !!linecolour)) +
-    geom_point()+
+    facets <- geom_line(aes(colour = !!linecolour)) +
     labs(title = "Percentage of Men in all regions of England of all age ranges who are current cigarette smokers aged 18 years and older",
          y = "Percentage(%)", x = "") +
     facet_grid(. ~ facet2)
-  
+    
+    return(facets)
 }
+
+
+
+
+ggplot(data = filtered_data,
+       aes(Year, Value)) +
+  geom_point() %+%
+  test_facet_args(filtered_data, facet_row, line_colour)
 
 #This function is for facet gridding. Will facet by row and coloumn
 test_facet_args_grid <- function(df, facet1, facet2, linecolour) {
@@ -30,10 +47,8 @@ test_facet_args_grid <- function(df, facet1, facet2, linecolour) {
            facet2 = !!(enquo(facet2)),
            linecolour = !!(enquo(linecolour)))
   
-  ggplot(data = df, 
-         aes(Year, Value))+
+    basic_plot_start +
     geom_line(aes(colour = !!linecolour)) +
-    geom_point()+
     labs(title = "Percentage of Men in all regions of England of all age ranges who are current cigarette smokers aged 18 years and older",
          y = "Percentage(%)", x = "") +
     facet_grid(facet1 ~ facet2)
