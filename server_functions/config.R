12181cc31b50cbde2b20043697120e01bd1295fd
Jakki <Varun.Jakki@ons.gov.uk>
2021-06-02 16:23:42 +0100

    Added new script which uses if else statements to create plot dependant on user choices (config file) - Basic plot + faceting + line_colour - Having issues sometimes when trying to run from ggplot_functions being sourced
    Signed-off-by: Jakki <Varun.Jakki@ons.gov.uk>

--- server_functions/config.R
+++ server_functions/config.R
@@ -1,14 +1,14 @@
 
 csv_filepath <- "Y:\\Data Collection and Reporting\\Jemalex\\CSV\\"
 indicator <- "indicator_3-a-1.csv"
-char1 <- "Sex"
-char2 <- "Country"
-char3 <- NA
-char4 <- NA
-levels1 <- c("Female", "Male")
-levels2 <- c("England")
-levels3 <- NA
-levels4 <- NA
+char1 <- "Country"
+char2 <- "Age"
+char3 <- "Sex"
+char4 <- "Region"
+levels1 <- c("England")
+levels2 <- c("18 to 24", "25 to 34")
+levels3 <- c("Male", "Female")
+levels4 <- c("London", "North East")
 line_colour <- "Sex"
-facet_row <- "Country"
-facet_column <- NA
+facet_row <- "Region"
+facet_column <- "Age"
