0faa3d6c266d39aefe22d1d856e9e5e93ebf71ba
Jakki <Varun.Jakki@ons.gov.uk>
2021-06-02 17:13:18 +0100

    Removed calling of plotting functions and if else statements in thsi script to new script Signed-off-by: Jakki <Varun.Jakki@ons.gov.uk>

--- server_functions/ggplot_function.R
+++ server_functions/ggplot_function.R
@@ -9,24 +9,16 @@ setwd("server_functions")
 
 source("config.R")   
 source("Filtering.R")
-source("plotting_functions.R")
+# source("plotting_functions.R")
 
 dat <- read.csv(paste0(csv_filepath, indicator), na.strings=c("","NA")) %>% 
   mutate_if(is.factor, as.character)
 
 filtered_data <- filter_for_selections(dat)
 
-#if only one facet selection the first function will run, if not the second will run.
-if(is.na(facet_row)||is.na(facet_column)){
-  test_facet_args(filtered_data,
-                  linecolour = line_colour, 
-                  facet2 = facet_row)
-} else {
-  test_facet_args_grid(df = filtered_data,
-                       linecolour = line_colour, 
-                       facet2 = facet_row,
-                       facet1 = facet_column)
-}
+# this is the script for plotting the data
+source("if_else_plotting_script.R")
+
 
 
 
