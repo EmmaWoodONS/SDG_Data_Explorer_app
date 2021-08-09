c1534ca90c99abedade9026c47246fa5c69a44ff
Jakki <Varun.Jakki@ons.gov.uk>
2021-08-09 22:16:56 +0100

    Download and Select Indicator button added and both work
    Positioning of these buttons should be changed
    
    Signed-off-by: Jakki <Varun.Jakki@ons.gov.uk>

--- Control_sheet/filtering_control_sheet/widget_test.R
+++ Control_sheet/filtering_control_sheet/widget_test.R
@@ -4,19 +4,26 @@ library(shinyWidgets)
 # data("mpg", package = "ggplot2")
 
 ui <- fluidPage(
+  tags$h2("Download bttn"),
+  downloadBttn(
+    outputId = "downloadData",
+    style = "bordered",
+    color = "primary"
+  ),
   fluidRow(
     column(
       width = 10, offset = 1,
       tags$h3("Filter data with selectize group"),
       panel(
+        uiOutput("Select Indicator"),
         selectizeGroupUI(
           id = "my-filters",
           params = list(
-            variable1 = list(inputId = "variable_group_1", title = "characteristic 1:"),
-            variable2 = list(inputId = "variable_group_2", title = "characteristic 2:"),
-            variable3 = list(inputId = "variable_group_3", title = "characteristic 3:"),
-            variable4 = list(inputId = "variable_group_4", title = "characteristic 4:")
-          )
+            variable1 = list(inputId = "variable1", title = "characteristic 1:"),
+            variable2 = list(inputId = "variable2", title = "characteristic 2:"),
+            variable3 = list(inputId = "variable3", title = "characteristic 3:"),
+            variable4 = list(inputId = "variable4", title = "characteristic 4:")
+          ),
         ), status = "primary"
       ),
       DT::dataTableOutput(outputId = "table")
@@ -32,9 +39,26 @@ server <- function(input, output, session) {
     module = selectizeGroupServer,
     id = "my-filters",
     data = control_sheet,
-    vars = c("variable_group_1", "variable_group_2", "variable_group_3", "variable_group_4")
+    vars = c("variable1", "variable2", "variable3", "variable4")
   )
-  output$table <- DT::renderDataTable(res_mod())
+  
+  
+  
+  output$`Select Indicator` <- renderUI({
+    
+      selectInput("Select Indicator", "Select Indicator", choices = c("All", unique(res_mod()$Indicator)))
+    
+  })
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste('data-', Sys.Date(), '.csv', sep='')
+    },
+    content = function(con) {
+      write.csv(res_mod(), con, row.names = FALSE)
+    }
+  )
+  
+  output$table <- DT::renderDataTable(res_mod(), rownames = FALSE)
 }
 
 shinyApp(ui, server)
