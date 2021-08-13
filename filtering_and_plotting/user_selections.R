bd1d4b7dd5bb690c84814da1176fcdac7beb5378
EmmaWoodONS <Emma.Wood@ons.gov.uk>
2021-08-13 18:09:03 +0100

    change images to jpg
    cant get pngs to display in the readme
    
    Signed-off-by: EmmaWoodONS <Emma.Wood@ons.gov.uk>

--- filtering_and_plotting/user_selections.R
+++ filtering_and_plotting/user_selections.R
@@ -5,7 +5,7 @@ indicator <- "3-4-1"
 
 char1 <- "Sex"
 char2 <- "Country" #"Sex"
-char3 <- "Age"
+char3 <- NA
 char4 <- NA
 
 
@@ -20,7 +20,7 @@ levels4 <- c(NA)
 
 line_colour <- "Sex"
 line_style <- NA
-facet_row <- "Age"#"Sex" #"Region"
+facet_row <- NA#"Age"#"Sex" #"Region"
 facet_column <- "Country"
 
 Units_selection <- NA
